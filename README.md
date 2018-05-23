8305c7cbe549bd9c074d90ee2b50e2c4b9e9cc82
Vedashree <l.vedashree@gmail.com>
2018-05-22 23:08:03 -0700

    Final Commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Photo Gallery using React 
 
-
+Version 1:
 This react application has the following structure :
 
 index.js (entry point)
@@ -13,3 +13,10 @@ Image.js (render individual images)
 
 
 Bootstrap has been used in addition to the normalized css . 
+
+
+
+Version 2: (using LightBox)
+LightBox has been used for neat animations of swiping between images .It also fits the requirements asked for this question where the images have to an array and the caption has to be over the image.
+
+By using Lightbox, the code is also optimized and there s just one component used .
--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -1,39 +1,74 @@
-import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Image from './Image';
+import React, { Component } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; 
+import './index.css';
+
+const images = [
+  {src: "https://tinyurl.com/pyrmds",
+   caption:"Great Pyramid of Giza"},
+   {src: "https://tinyurl.com/twrpsa",
+   caption:"Leaning Tower of Pisa"},
+   {src: "https://tinyurl.com/tjmhl",
+   caption:"TajMahal "},
+   {src: "https://tinyurl.com/grtwlchn",
+   caption:"The Great Wall of China "},
+   {src: "https://tinyurl.com/clssmrmtly",
+   caption:"The Colosseum in Rome"},
+   {src: "https://tinyurl.com/mchupcchu",
+   caption:"Machu Picchu "}  
+    
+];
+
+
 
 // Component for PhotoGallery
 export default class PhotoGallery extends React.Component{
  constructor(props) {
-  super(props);
-  this.state = {
-   url: ''
+    super(props);
+ 
+    this.state = {
+      element: 0,
+      isOpen: false,
+    };
   }
- }
  
- render() {
-  return(
-    //container-fluid takes care of alignment
-    //col-sm-6 col-md-3 col-xl-2 is for the grid layout of the images
-   <div refs='gallery-container' className='container-fluid gallery-container'>
-   <h1 style={{height: 150, backgroundColor:'black', textAlign:'center',color:'white',verticalAlign: 'middle',paddingTop:40}}> Photo Gallery - Wonders of the World </h1>
-   <div className='row'>
-    {
-     this.props.images.map((url, index) => {
-      return <div className='col-sm-6 col-md-3 col-xl-2'>
-       <div >
-        <Image className='img' src={url.src} alt={'Image number ' + (index + 1)} caption={url.caption} />
+  render() {
+    const { element, isOpen } = this.state;
  
-       </div>
-      </div>
-     })
-    }
-   </div>
+    return (
+      <div id="container">
+        <button className="box" type="button"
+                    onClick={() => this.setState({ isOpen: true })} ><span>Click to open my photo gallery </span></button>
 
-  </div>
-  )
- }
+ //Using inbuilt lightbox capabilities
+        {isOpen && (
+          <Lightbox
+            imageCaption={images[element].caption}
+            mainSrc={images[element].src}
+            
+            nextSrc={images[(element + 1) % images.length].src}
+            prevSrc={images[(element + images.length - 1) % images.length].src}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                element: (element + images.length - 1) % images.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                element: (element + 1) % images.length,
+              })
+            }
+          />
+        )}
+      </div>
+    );
+  }
 }
+
+
+
  
  
 
